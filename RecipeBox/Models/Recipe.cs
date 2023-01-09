61999194ad00d43964cb07a4b34551d38ee17cf8
Alex Johnson <alex.johnson293@gmail.com>
2023-01-09 13:24:40 -0800

    initialize project
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

---
+++ RecipeBox/Models/Recipe.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace RecipeBox.Models
+{
+  public class Recipe
+  {
+    public int RecipeId { get; set; }
+    // [Required(ErrorMessage = "The recipe's name can't be empty!")]
+    public string Name { get; set; }
+    public string Ingredients { get; set; }
+    public string Instructions { get; set; }
+    public int Rating { get; set; }
+    public List<RecipeTag> JoinEntities { get;}
+  }
+}
